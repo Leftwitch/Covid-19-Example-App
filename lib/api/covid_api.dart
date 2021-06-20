d5aa6489eb1d545c46db9052b7397682ad006151
Justin Guckes <justin.guckes@gmail.com>
2021-06-20 14:06:00 +0200

    Add Covid-API

--- lib/api/covid_api.dart
+++ lib/api/covid_api.dart
@@ -1,2 +1,16 @@
-
 // API URL: https://api.covid19api.com/summary
+
+import 'dart:convert';
+
+import 'package:google_maps_example/model/covid_summary_entry.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<CovidSummaryEntry>> getCovidSummary() async {
+  final url = Uri.parse('https://api.covid19api.com/summary');
+
+  final response = await http.get(url);
+
+  return List<CovidSummaryEntry>.from(json
+      .decode(response.body)["Countries"]
+      .map((x) => CovidSummaryEntry.fromJson(x)));
+}
